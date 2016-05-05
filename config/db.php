9ff7dc18c38f55114d4c52f12a72ba14fe4f112b
unid3ntified <unid3ntifiedz@gmail.com>
2016-05-05 19:40:28 +0700

    initialization

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=tugas3sil',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
